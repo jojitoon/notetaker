b1b7213b186053146630f7b6822ddf89ddf36513
Jojitoon <jojitoon@gmail.com>
2020-03-03 11:25:20 +0100

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { useEffect, useState } from 'react';
+
+import logo from './logo.svg';
+
+type Note = {
+    body: string;
+    date: Date;
+};
+const App: React.FC = () => {
+    const [value, setValue] = useState('');
+    const [notes, setNotes] = useState<[Note] | Note[] | null>(null);
+
+    useEffect(() => {
+        const notes = localStorage.getItem('allnotes');
+        if (notes) {
+            setNotes(JSON.parse(notes));
+        }
+    }, []);
+    useEffect(() => {
+        localStorage.setItem('allnotes', JSON.stringify(notes));
+    }, [notes]);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div>Note Taker</div>
+                <img src={logo} className="App-logo" alt="logo" />
+                <span>use shift + enter to add new line</span>
+                <textarea
+                    className="input"
+                    rows={4}
+                    value={value}
+                    onChange={e => {
+                        setValue(`${e.target.value}`);
+                    }}
+                    onKeyUp={event => {
+                        if (event.key === 'Enter') {
+                            if (event.shiftKey) {
+                                return null;
+                            } else {
+                                if (value.trim() && value.trim().length) {
+                                    const newvalue: Note = {
+                                        body: value,
+                                        date: new Date(),
+                                    };
+                                    const upload: [Note] | Note[] = notes ? [...notes] : [];
+                                    upload.unshift(newvalue);
+                                    setNotes(upload);
+                                    setValue('');
+                                }
+                            }
+                        }
+                    }}
+                />
+
+                <div className="container">
+                    {notes &&
+                        notes.map((note, i) => (
+                            <div className="notes" key={i}>
+                                {note.body}
+                                <span>{new Date(note.date).toDateString()}</span>
+                            </div>
+                        ))}
+                </div>
+            </header>
+            <div className="foot">NoteTaker - by joji @2020</div>
+        </div>
+    );
+};
 
 export default App;
